38e7fc7fd7c83df81c2e339d5b59a1c3196e3201
Boby900 <bobytiwari310@gmail.com>
2024-03-02 17:56:34 +0530

    adding Stripe to the Application

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,21 +4,30 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "@/app/theme-provider"
 import { PropsWithChildren } from "react";
-
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+import CheckoutForm from "@/components/ui/checkoutForm";
+const stripePromise = loadStripe('pk_test_51MTGWkSCJLPdp7E7LV2XHIjGEG6p7MqJzuZFkouznskmNTUq9VuCPmrYuuhtGA49ERXcEUotMbzoKZkGfTOrqEdX00mAopRyOL');
 const convex = new ConvexReactClient(
   process.env.NEXT_PUBLIC_CONVEX_URL as string
 );
 
 
 export function Providers({ children }: PropsWithChildren) {
-
+  const id = ''
+  console.log(process.env.STRIPE_SECRET_KEY)
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: id}
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
      
-
+      <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm />
+    
       <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -27,6 +36,7 @@ export function Providers({ children }: PropsWithChildren) {
           >
         {children}
         </ThemeProvider>
+        </Elements>
         
       </ConvexProviderWithClerk>
     </ClerkProvider>
