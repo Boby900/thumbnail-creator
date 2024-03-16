bfe8ae723db7060c2053e73009edc34fa4f9caf0
Boby900 <bobytiwari310@gmail.com>
2024-03-16 15:40:21 +0530

    added hamburger menu

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,29 +2,84 @@
 
 import { SignInButton } from "@clerk/nextjs";
 import { ModeToggle } from "./mode-toggle";
-import { SignedIn, SignedOut, UserButton,useAuth } from "@clerk/clerk-react";
-import  Link  from "next/link";
+import { SignedIn, SignedOut, UserButton, useAuth } from "@clerk/clerk-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
-  const redirectUrl = useAuth()
+  const redirectUrl = useAuth();
   return (
     <div className="border-b-2 border-slate-300 ">
-      <div className="h-16  container flex justify-between items-center ">
-        <div><Link href="/" className="link ">ThumbnailCreator</Link></div>
+      <div className="h-16  container  flex justify-between items-center ">
+        <div className="sm:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Menu</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Main Menu</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Link href={"/"}>Home</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={"/pricing"}>Pricing</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href={"/about"}>About</Link>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <div>
+          <Link href="/" className="link hidden  sm:block">
+            ThumbnailCreator
+          </Link>
+        </div>
         <div className="flex gap-4 items-center justify-between  w-[10%]">
           <SignedIn>
-            <Link href="/dashboard" className="link">Dashboard</Link>
-            <Link href="/create" className="link">Create</Link>
+            <Link href="/dashboard" className="link hidden sm:block">
+              Dashboard
+            </Link>
+            <Link href="/create" className="link hidden sm:block">
+              Create
+            </Link>
           </SignedIn>
           <SignedOut>
-            <Link href="/pricing" className="link">Pricing</Link>
-            <Link href="/about" className="link">About</Link>
+            <Link href="/pricing" className="link hidden sm:block">
+              Pricing
+            </Link>
+            <Link href="/about" className="link hidden sm:block">
+              About
+            </Link>
           </SignedOut>
         </div>
         <div className="flex items-center gap-2">
-        <SignedIn><UserButton afterSignOutUrl={'/'} /></SignedIn>
-        <SignedOut><SignInButton/></SignedOut>
-        <ModeToggle />
+          <SignedIn>
+            <UserButton afterSignOutUrl={"/"} />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <ModeToggle />
         </div>
       </div>
     </div>
