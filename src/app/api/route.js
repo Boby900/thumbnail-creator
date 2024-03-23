f23b548af7eead700de13cf98ebc00bdda3a704a
Boby900 <bobytiwari310@gmail.com>
2024-03-23 16:47:06 +0530

    added nextresponse

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,28 +1,24 @@
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-      res.redirect(303, session.url);
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+export async function POST(req) {
+  const origin = new URL(req.url).origin;
+  try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: "{{PRICE_ID}}",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `${origin}/?success=true`,
+      cancel_url: `${origin}/?canceled=true`,
+    });
+    return NextResponse.redirect(session.url, { status: 303 });
+  } catch (err) {
+    return new NextResponse(err.message, { status: 500 });
   }
 }
