4ce001f59ce58e55a80801775500375342a32cd3
Boby900 <bobytiwari310@gmail.com>
2024-03-23 17:30:22 +0530

    removed unnecessary imports

--- src/app/stripe/page.tsx
+++ src/app/stripe/page.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { useState } from 'react';
-import POST from '../api/route';
+
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every rende
-import { NextResponse } from 'next/server'
+
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
